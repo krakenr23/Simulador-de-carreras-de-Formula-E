de72dd275688607017a08947604ec169fc2f39b2
krakenr23 <146369538+krakenr23@users.noreply.github.com>
2024-11-14 14:22:53 -0600

    Update SimuladorCarrera.c
    Creación del proyecto

--- SimuladorCarrera.c
+++ SimuladorCarrera.c
@@ -1,1 +1,24 @@
+/**
+@file SimuladorCarrera.c
+@brief Simulador de Carreras de formuloa E
+@details Programa que nos permite hacer simular una carrera con un maximo de 22 pilotos, 10 equipos, 10 paises diferentes y 5 carreras
+@author Pograma desarrollado por: 
+Carlos Eduardo Ríos
+Andres Muñoz Lozano
+Daniel Alejando Loyo
+@date Fecha de creacion: 14 de Noviembre del 2024
+@author Ultimo en modificar: Carlos Eduardo Ríos
+@date Fecha ultima de modificacion: 14 de Noviembre del 2024
+ */
 
+#include "/home/ict24crv/GrupoC/FundProgra/MisBibliotecas/MiBiblioteca.h"
+
+#define Max_Pilotos 22
+#define Max_Equipos 10
+#define Max_Paises 10
+#define Max_Carreras 5
+
+int main(void){
+
+  return 0;
+}
